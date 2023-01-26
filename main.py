753e9ce1b5c1c33a5aed9f590fd991fe63b0234d
mbarneto <mattdokn@gmail.com>
2023-01-25 20:43:54 -0500

    Setting up database creation

--- main.py
+++ main.py
@@ -1,7 +1,93 @@
-from flask import Flask
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
 
-app = Flask(__name__)
+import sqlite3
+from sqlite3 import Error
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+def init_db():
+    print(len('barnetomatthew@gmail.com'))
+    conn = None
+    try:
+        # Get our db connection
+        conn = sqlite3.connect('storage.db')
+        # Create our tables if they don't already exist
+        # TODO: figure out length limits and fix data type/***attributes***
+        conn.execute("""CREATE TABLE users (
+            user_id INT NOT NULL PRIMARY KEY,
+            username CHAR[32],
+            password CHAR[32],
+            email CHAR[255],
+            phone_number CHAR[32],
+            degree CHAR[32],
+            semester CHAR[32]);
+        """)
+
+        conn.execute("""CREATE TABLE courses (
+            course_id INT NOT NULL PRIMARY KEY,
+            name CHAR[32],
+            section CHAR[32],
+            professor_name CHAR[32],
+            online BOOL,
+            dropped BOOL,
+            color CHAR[6]);
+        """)
+
+        # Shouldn't weight just be based on the type of assignment?
+        conn.execute("""CREATE TABLE assignments (
+            assignment_id INT NOT NULL PRIMARY KEY,
+            course_id INT,
+            name CHAR[32],
+            type CHAR[32],
+            weight CHAR[32],
+            priority INT,
+            completed BOOL,
+            due DATETIME,
+            recurring BOOL,
+            notification_id INT);
+        """)
+
+        conn.execute("""CREATE TABLE subassignments (
+            subassignment_id INT NOT NULL PRIMARY KEY,
+            assignment_id INT,
+            name CHAR[32],
+            desc TEXT,
+            completed BOOL,
+            due DATETIME,
+            recurring BOOL,
+            notification_id INT);
+        """)
+        
+        conn.execute("""CREATE TABLE notifications (
+            notification_id INT NOT NULL PRIMARY KEY,
+            message TEXT,
+            delivery_method INT,
+            send_at DATETIME,
+            assignment_id INT,
+            subassignment_id INT);
+        """)
+
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
+
+async def get_req(req):
+    return JSONResponse({'aaaaaa': 'bbbbbbbbb'})
+
+async def post_req(req):
+    return JSONResponse({'cccc': 'ddddddd'})
+
+init_db()
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+    Route('/req', endpoint=post_req, methods=['POST']),
+    Route('/req', endpoint=get_req, methods=['GET'])
+])
