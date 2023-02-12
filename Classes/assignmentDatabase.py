568bc218458944230699984b5ffd58bb3394f49f
WakeTechBrandon <114441427+WakeTechBrandon@users.noreply.github.com>
2023-02-11 20:32:27 -0500

    Update assignmentDatabase.py
    Added add_row and remove_row (placeholder) functionality for database class. Also updated documentation on functions.

--- Classes/assignmentDatabase.py
+++ Classes/assignmentDatabase.py
@@ -1,8 +1,11 @@
 import sqlite3
 from sqlite3 import Error
 
-# Manager model for handling all assignments added by the user.
+
+# Encapsulates access to the sql database
 class AssignmentDatabase:
+
+    """ Default Function Section """
 
     # initialize function
     # formally: init_db()
@@ -95,15 +98,91 @@ class AssignmentDatabase:
             if conn:
                 conn.close()
 
-    # Utility function for adding another row to the assignments table
+    """ Row Modification Section """
+    """         Adding           """
+
+    # SQL pass function to add new row to USERS table
+    def add_row_users(self, user_id, username, password, email, phone_number, degree, semester):
+        """CREATE TABLE IF NOT EXISTS users (
+                user_id INT PRIMARY KEY,
+                username CHAR[32] UNIQUE,
+                password CHAR[32],
+                email CHAR[255],
+                phone_number CHAR[32],
+                degree CHAR[32],
+                semester CHAR[32]);
+            """
+        conn = None
+        try:
+            conn = sqlite3.connect('storage.db')
+            # Should there be a semicolon after the VALUES() list?
+            conn.execute('''
+                INSERT INTO 
+                users(user_id, username, password, email, phone_number, degree, semester) 
+                VALUES(?,?,?,?,?,?,?)
+            ''', (user_id, username, password, email, phone_number, degree, semester))
+
+            conn.commit()
+        except Error as e:
+            print(e)
+        finally:
+            if conn:
+                conn.close()
+
+    # SQL pass function to add new row to COURSES table
+    def add_row_courses(self, course_id, name, section, professor_name, online, dropped, color):
+        """CREATE TABLE IF NOT EXISTS courses (
+                course_id INT NOT NULL PRIMARY KEY,
+                name CHAR[32],
+                section CHAR[32],
+                professor_name CHAR[32],
+                online BOOL,
+                dropped BOOL,
+                color CHAR[6]);
+            """
+        conn = None
+        try:
+            conn = sqlite3.connect('storage.db')
+            # Should there be a semicolon after the VALUES() list?
+            conn.execute('''
+                INSERT INTO 
+                users(course_id, name, section, professor_name, online, dropped, color) 
+                VALUES(?,?,?,?,?,?,?)
+            ''', (course_id, name, section, professor_name, online, dropped, color))
+
+            conn.commit()
+        except Error as e:
+            print(e)
+        finally:
+            if conn:
+                conn.close()
+
+    # SQL pass function to add new row to ASSIGNMENTS table
+    # def add_row_assignments(self):
+    #    pass
+    # @todo Add default function with parameters. Overloading in Python doesn't exist and I don't want to have a bunch of different named functions doing the same thing.
+    #
+    # Utility function for adding another row to the assignments table using Assignment class
     def add_row_assignments(self, assignment):
+        """CREATE TABLE IF NOT EXISTS assignments (
+                assignment_id INT NOT NULL PRIMARY KEY,
+                course_id INT,
+                name CHAR[32],
+                type CHAR[32],
+                weight CHAR[32],
+                priority INT,
+                completed BOOL,
+                due DATETIME,
+                recurring BOOL,
+                notification_id INT);
+            """
         conn = None
         try:
             conn = sqlite3.connect('storage.db')
             conn.execute('''
                 INSERT INTO 
                 assignments(assignment_id, course_id, name, type, weight, priority, completed, due, recurring, notification_id) 
-                VALUES(?,?,?,?,?,?,?,?,?,?)
+                VALUES(?,?,?,?,?,?,?,?,?,?,?)
             ''', (assignment.get_id(),
                   assignment.get_course_id(),
                   assignment.get_name(),
@@ -122,3 +201,89 @@ class AssignmentDatabase:
         finally:
             if conn:
                 conn.close()
+
+    # SQL pass function to add new row to SUBASSIGNMENTS table
+    # def add_row_subassignments(self):
+    #    pass
+    # @todo Add default function with parameters. Overloading in Python doesn't exist and I don't want to have a bunch of different named functions doing the same thing.
+    #
+    # Utility function for adding another row to the subassignments table using Assignment class
+    # SQL pass function to add new row to SUBASSIGNMENTS table
+    def add_row_subassignments(self, subassignment_id, assignment):
+        """CREATE TABLE IF NOT EXISTS subassignments (
+                subassignment_id INT NOT NULL PRIMARY KEY,
+                assignment_id INT,
+                name CHAR[32],
+                desc TEXT,
+                completed BOOL,
+                due DATETIME,
+                recurring BOOL,
+                notification_id INT);
+            """
+        conn = None
+        try:
+            conn = sqlite3.connect('storage.db')
+            conn.execute('''
+                INSERT INTO 
+                subassignments(subassignment_id, assignment_id, course_id, name, type, weight, priority, completed, due, recurring, notification_id) 
+                VALUES(?,?,?,?,?,?,?,?,?,?)
+            ''', (subassignment_id,
+                  assignment.get_id(),
+                  assignment.get_course_id(),
+                  assignment.get_name(),
+                  assignment.get_type(),
+                  assignment.get_weight(),
+                  assignment.get_priority(),
+                  assignment.get_completed(),
+                  assignment.get_due(),
+                  assignment.get_recurring(),
+                  assignment.get_notification_id()
+                  ))
+
+            conn.commit()
+        except Error as e:
+            print(e)
+        finally:
+            if conn:
+                conn.close()
+
+    # SQL pass function to add new row to NOTIFICATIONS table
+    def add_row_notifications(self, notification_id, message, delivery_method, send_at, assignment_id, subassignment_id):
+        """CREATE TABLE IF NOT EXISTS notifications (
+                notification_id INT NOT NULL PRIMARY KEY,
+                message TEXT,
+                delivery_method INT,
+                send_at DATETIME,
+                assignment_id INT,
+                subassignment_id INT);
+            """
+        conn = None
+        try:
+            conn = sqlite3.connect('storage.db')
+            conn.execute('''
+                INSERT INTO 
+                assignments(notification_id, message, delivery_method, send_at, assignment_id, subassignment_id) 
+                VALUES(?,?,?,?,?,?)
+            ''', (notification_id, message, delivery_method, send_at, assignment_id, subassignment_id))
+
+            conn.commit()
+        except Error as e:
+            print(e)
+        finally:
+            if conn:
+                conn.close()
+
+    """ Row Modification Section """
+    """        Removing          """
+
+    def remove_row_users(self):
+        pass
+    def remove_row_courses(self):
+        pass
+    def remove_row_assignments(self):
+        pass
+    def remove_row_subassignments(self):
+        pass
+    def remove_row_notifications(self):
+        pass
+
