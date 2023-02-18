9829a24611b1e718265536c51760ee21ccb02277
William Greco <wsgreco@my.waketech.edu>
2023-02-18 10:51:18 -0500

    Added comments

--- get_complete.py
+++ get_complete.py
@@ -1,5 +1,7 @@
 from sqlite3 import connect
 
+# Added ability to filter by course or for sub assignments by parent assignment
+
 
 def get_completed_assignments(course_id: int = None) -> list:
     con = connect("storage.db")
@@ -9,6 +11,7 @@ def get_completed_assignments(course_id: int = None) -> list:
     if course_id is not None:
         query.append(f"AND course_id = ?")
 
+    # Unpack results in to assignment class
     results = con.execute("".join(query), (course_id,)).fetchall()
     con.close()
     return results
@@ -25,6 +28,7 @@ def get_completed_sub_assignments(assignment_id: int = None, course_id: int = No
     if course_id is not None:
         query.append(" AND course_id = ?")
 
+    # Unpack results in to sub_assignment class
     results = con.execute("".join(query), (assignment_id, course_id)).fetchall()
     con.close()
     return results
