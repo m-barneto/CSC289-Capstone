8739a10b4b079aa42f61ff14ce6ce914c6c50855
WakeTechBrandon <114441427+WakeTechBrandon@users.noreply.github.com>
2023-03-05 19:16:25 -0500

    Update notification.py
    Updated documentation and added NotificationDelegate class decoration. The delegate will go out when Notification.send_notification() is called. The Notification.send_notification() is currently called from a separate thread that is always checking if the notification date is the current date. The AssignmentManager should attach to this delegate and call Notification.stop_tick() if the assignment is non-recurring, otherwise the date should be set to 1 week (should this be a variable? How often the assignment recurrs?) from the current date.

--- Classes/notification.py
+++ Classes/notification.py
@@ -3,19 +3,81 @@ from datetime import datetime
 from threading import Thread, Event
 
 
-# Create class that checks if current date matches the notification date
-# After initializing, call start_tick() to begin checking if the 
-# Notification should be sent.
+class NotificationDelegate:
+    """
+    A class decorator intended to be used on functions.
+    You may call the function as normal, for Example:
+
+    @NotificationDelegate
+    def foo():
+        pass
+
+    foo() # This will call the function foo()
+
+    Other objects may bind their functions to this function
+    And be called in sequence as a result. For Example:
+
+    @NotificationDelegate
+    def foo():
+        pass
+
+    class Bar:
+        def do_something(self):
+            pass
+
+    bar = Bar()
+    foo.add_event(bar.do_something)
+    foo() # This will call foo() and then bar.do_something()
+    """
+
+    def __init__(self, func):
+        """Constructor for delegate object."""
+
+        self._func = func
+        self._events = list()
+
+    def __call__(self):
+        """() override to call __call__() on all bound events."""
+
+        self._func()
+        self.broadcast()
+
+    def broadcast(self):
+        """Calls all bound events."""
+
+        for event in self._events:
+            event()
+
+    def add_event(self, event):
+        """Adds an event to be called when this object has been called."""
+
+        self._events.append(event)
+
+
 class Notification:
+    """
+    Class that checks if current date matches the notification date.
+    After initializing, call start_tick() to begin checking if the
+    Notification should be sent.
+    """
 
     """
-    param: Notification_id : int
-    param: SubAssignment_id : int
-    param: Message : string
-    param: send_date : datetime
-    param: tick_frequency : float
+    Function Section
+    ======= Defaults =======
     """
+
     def __init__(self, notification_id, subassignment_id, message, send_date=datetime, tick_frequency=60.0): # Checks every minute by default
+        """
+        Constructor for Notification class
+
+        Parameters:
+            notification_id : int
+            sub_Assignment_id : int
+            message : string
+            send_date : datetime
+            tick_frequency : float
+        """
+
         self._id = notification_id
         self._assignment_id = subassignment_id
         self._previous_time = datetime.now().time()
@@ -24,11 +86,14 @@ class Notification:
         self._tick_thread = Thread(target=self._perform_tick())
         self._stop_tick_event = Event()
 
-    """ ==== Getters ==== """
+    """
+    Function Section
+    ==== Getters ====
+    """
 
     def get_id(self):
         return self._id
-    
+
     def get_assignment_id(self):
         return self._assignment_id
 
@@ -41,11 +106,14 @@ class Notification:
     def get_date(self):
         return self._date
 
-    """ ==== Mutators ==== """
+    """
+    Function Section
+    ==== Mutators ====
+    """
 
     def set_id(self, new_id):
         self._id = new_id
-        
+
     def set_subassignment_id(self, new_id):
         self._assignment_id = new_id
 
@@ -55,14 +123,23 @@ class Notification:
     def set_date(self, date):
         self._date = date
 
-    """ ==== Ticking ==== """
+    """
+    Function Section
+    ==== Ticking ====
+    """
 
-    # Start notification thread
     def start_tick(self):
+        """
+        Starts notification thread.
+        An infinite loop checks if enough time has elapsed
+        To check if the send_date has been reached. There is
+        An exit event in case of early shutdown as well.
+        """
         self._tick_thread.start()
 
-    # Thread function to call at frequency amount
     def _perform_tick(self):
+        """Thread function to call at frequency amount."""
+
         while True:
             delta_time = datetime.now().time() - self._previous_time
             if delta_time > self._frequency:
@@ -71,17 +148,27 @@ class Notification:
             if self._stop_tick_event.is_set():
                 break
 
-    # Event called to check if notification should be sent out
     def _tick(self, delta_time):
+        """Event called to check if notification should be sent out"""
+
         if self._date >= datetime.now().date():
             self.send_notification()
 
-    # Event to stop tick thread
     def stop_tick(self):
+        """Event to stop tick thread."""
+
         self._stop_tick_event.set()
         self._tick_thread.join()
 
-    # Event called when notification parameters have been met.
-    # Should send a phone and email notification.
+    @NotificationDelegate
     def send_notification(self):
+        """
+        Event called when notification parameters have been met.
+        Should send a phone and email notification.
+
+        This has been decorated with a delegate decoration.
+        This means that other functions can bind themselves to
+        This function and be called after this Event is called.
+        """
+
         pass
